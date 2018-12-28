589c26cdf50d7fad0824e5c35cc335cfdfd3d120
kevinche <kevinche@umich.edu>
2018-12-28 15:07:34 -0500

    email3

--- contact.php
+++ contact.php
@@ -1,31 +1,18 @@
+
 <?php
-if (isset($_POST['submit']) && isset($_POST['name']) && isset(_POST['email'])) {
-$name = $_POST['name'];
-$email = $_POST['email'];
 
-//send mail
-$to = 'kevin.c388@gmail.com';
-$subject = "Test website";
-$body = '<html> 
-            <body>
-                <p>Name: '.$name.'</p>
-                <br>
-                <p>Email: '.$email.' </p>
-            </body>
-        </html>';
- $headers  ="From:".$name."<".$email.">\r\n";
- $headers .="reply-To:".$email."\r\n";
- $headers .="NINE-Version: 1.0\r\n";
- $headers .="Content-type: text/html; charset=utf-8";
+if($_POST["submit"]) {
+    $recipient="your@email.address";
+    $subject="Form to email message";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
 
-//sending process
-$send = mail($to, $subject, $body, $headers);
-if ($send) {
-    echo "<br>";
-    echo "Thanks for contacting us! We will get back to you shortly.";
-} else {
-    echo "error";
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
-}
+
 ?>
-
